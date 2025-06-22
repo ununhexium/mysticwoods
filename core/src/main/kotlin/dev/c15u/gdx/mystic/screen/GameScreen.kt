e8dd8812cce3f9ba415777591e228544f101a2b5
Christophe '116' Loiseau <uuh116@gmail.com>
2025-06-22 21:42:34 +0200

    Part 04 (TextureRegion & TextureAtlas)

--- core/src/main/kotlin/dev/c15u/gdx/mystic/screen/GameScreen.kt
+++ core/src/main/kotlin/dev/c15u/gdx/mystic/screen/GameScreen.kt
@@ -1,9 +1,10 @@
 package dev.c15u.gdx.mystic.screen
 
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.Disposable
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import com.github.quillraven.fleks.configureWorld
 import dev.c15u.gdx.mystic.component.ImageComponent
@@ -14,8 +15,10 @@ import ktx.log.logger
 
 class GameScreen : KtxScreen {
 
-    private val texture = Texture("assets/graphics/player.png")
-    private val stage = Stage(ExtendViewport(16f, 9f))
+    private val disposables = ArrayList<Disposable>()
+
+    private val textureAtlas = TextureAtlas("assets/graphics/gameObjects.atlas").alsoDispose()
+    private val stage = Stage(ExtendViewport(16f, 9f)).alsoDispose()
     private val world = configureWorld {
         injectables {
             add(stage)
@@ -34,18 +37,21 @@ class GameScreen : KtxScreen {
     }
 
     override fun show() {
-
         world.entity {
-            it += ImageComponent(Image(texture).apply { setSize(4f, 4f) })
+            it += ImageComponent(
+                Image(TextureRegion(textureAtlas.findRegion("player"), 0, 0, 48, 48)).apply {
+                    setSize(4f, 4f)
+                }
+            )
         }
-
-        stage.addActor(
-            Image(texture).apply {
-                setPosition(1f, 1f)
-                setSize(1f, 1f)
-                setScaling(Scaling.fill)
-            }
-        )
+        world.entity {
+            it += ImageComponent(
+                Image(TextureRegion(textureAtlas.findRegion("slime"), 0, 0, 32, 32)).apply {
+                    setSize(4f, 4f)
+                    setPosition(12f, 1f)
+                }
+            )
+        }
     }
 
     override fun resize(width: Int, height: Int) {
@@ -60,8 +66,13 @@ class GameScreen : KtxScreen {
     }
 
     override fun dispose() {
-        stage.disposeSafely()
-        texture.disposeSafely()
+        disposables.forEach { it.disposeSafely() }
         world.dispose()
     }
+
+    private fun <T : Disposable> T.alsoDispose(): T {
+        disposables.add(this)
+        return this
+    }
+
 }
